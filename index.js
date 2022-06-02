6fb2231a776c3e410defedcb78ecd415d8c13aef
Dennis Mwangi <dennis.kamau@student.moringaschool.com>
2022-06-02 23:06:00 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+let names = ["Guadalupe", "Ollie", "Aki"]
+let birthday = "suprise"
+
+function writeCards(names,birthday){
+     let thankYouCards = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${birthday} gift!`) 
+        
+    }
+    return thankYouCards
+}
+function countDown(count){
+    
+    while(count>0){
+        console.log(count)
+        count --;
+    };
+    console.log(count);
+}
